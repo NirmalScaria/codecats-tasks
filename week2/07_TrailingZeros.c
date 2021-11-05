ca05c0028a4c41cd6e62ff19058bb21531d08212
NirmalScaria <alreadynirmalised@gmail.com>
2021-11-06 00:58:17 +0530

    TrailingZeros

---
+++ week2/07_TrailingZeros.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int t,x,result=0;
+    scanf("%d",&t);
+    while(t){
+        --t;
+        scanf("%d",&x);
+        result=0;
+        while(x){
+            x/=5;
+            result+=x;
+        }
+        printf("%d\n",result);
+    }
+    return(0);
+}
