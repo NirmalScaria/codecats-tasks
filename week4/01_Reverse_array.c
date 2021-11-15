5ee040ccc8ef965674a317adc2be492df448b045
NirmalScaria <alreadynirmalised@gmail.com>
2021-11-15 10:56:44 +0530

    ReverseArray

---
+++ week4/01_Reverse_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int arr[100],i=1,count;
+    printf("Enter number of items : ");
+    scanf("%d",&count);
+    printf("Enter the elements.\n");
+    for(i=0;i<count;i++){
+        scanf("%d",&arr[i]);
+    }
+    // Swapping the last element with first element, and so on.
+    int l=0, r=count-1, temp;
+    while(l<r){
+        temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        l++;
+        r--;
+        printf("Swapped 1");
+    }
+    printf("Reversed array is : \n");
+    for(i=0; i<count; i++){
+        printf("%d\n", arr[i]);
+    }
+}
